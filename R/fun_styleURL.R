dcecfd6373504352b15b31a9583a5f262b088d72
Tingting Zhan <tingtingzhan@gmail.com>
2024-09-07 18:47:36 -0400

    test

--- R/fun_styleURL.R
+++ R/fun_styleURL.R
@@ -20,13 +20,21 @@ styleURL <- function(url_, text_) {
   if (!n) return(invisible())
   
   if (!is.character(url_) || anyNA(url_) || !all(nzchar(url_))) stop('illegal `url_`')
-  id <- !grepl('^https://|^http://', x = url_)
-  if (any(id)) url_[id] <- paste0('https://', url_[id])
   
-  if (missing(text_) || !length(text_)) text_ <- gsub('^https://|^http://', replacement = '', x = url_)
+  url_ok <- grepl(pattern = '\033]8;', x = url_)
+  if (all(url_ok)) return(url_)
+  
+  url__ <- url_[!url_ok]
+  id <- !grepl('^https://|^http://', x = url__)
+  if (any(id)) url__[id] <- paste0('https://', url__[id])
+  
+  if (missing(text_) || !length(text_)) {
+    text_ <- gsub('^https://|^http://', replacement = '', x = url__)
+  }
   if (!is.character(text_) || length(text_) != n || anyNA(text_) || !all(nzchar(text_))) stop('illegal reference name')
   
-  tmp <- style_hyperlink(text = text_, url = url_)
-  return(unclass(tmp))
+  url_[!url_ok] <- style_hyperlink(text = text_, url = url__)
+  #return(unclass(url_))
+  return(url_)
 
 }
