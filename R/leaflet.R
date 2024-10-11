f6d4c7ca4e9665a3636ddbe44c783b61f6ed93af
Tingting Zhan <tingtingzhan@gmail.com>
2024-10-10 20:20:18 -0400

    test

--- R/leaflet.R
+++ R/leaflet.R
@@ -23,21 +23,23 @@
 #' @references
 #' \url{http://rstudio.github.io/leaflet/}
 #' \url{https://developers.google.com/maps/documentation/geocoding/}
-#' \url{http://shiny.rstudio.com/gallery/tags/leaflet/}
+#' \url{http://shiny.rstudio.com/gallery}
 #'
 #' @export
 as.leaflet <- function(x, ...) UseMethod('as.leaflet')
 
 #' @export
-as.leaflet.IATA <- function(x, ...) as.leaflet.SpatialPoints(airports_ip2location[x[[1L]], , drop = FALSE])
+as.leaflet.IATA <- function(x, ...) {
+  ap <- airports_ip2location[x[[1L]], , drop = FALSE] # 'SpatialPoints'
+  leaflet_popup(coords = ap@coords, popup = ap$iata, ...)
+}
+
 
 #' @export
 as.leaflet.SpatialPoints <- function(x, ...) leaflet_popup(coords = x@coords, ...)
 
 # still playing
 # as.leaflet.SpatialPolygons <- (PACovid19)
-
-
 
 
 #' @title \link[leaflet]{leaflet} with Popup
@@ -48,27 +50,30 @@ as.leaflet.SpatialPoints <- function(x, ...) leaflet_popup(coords = x@coords, ..
 #' @param coords 2-column \link[base]{matrix} of popup 
 #' *longitude* (1st column) and *latitude* (2nd column)
 #' 
+#' @param popup \link[base]{character} \link[base]{vector},
+#' popup text.  Default is the \link[base]{rownames} of `coords`
+#' 
 #' @param ... additional parameters, currently not in use
+#' 
+#' @returns
+#' Function [leaflet_popup] returns an \link[leaflet]{leaflet} object, which inherits from \CRANpkg{htmlwidgets}.
 #' 
 #' @importFrom leaflet leaflet addPopups popupOptions addTiles fitBounds
 #' @export
-leaflet_popup <- function(
-  coords,
-  ...
-) {
+leaflet_popup <- function(coords, popup = rownames(coords), ...) {
   
   if (!is.matrix(coords) || !is.numeric(coords) || anyNA(coords) || dim(coords)[2L] != 2L) stop('coords must be coords')
   
-  popup <- dimnames(coords)[[1L]] # rownames of `coords` as popup labels
   if (!length(popup) || anyNA(popup) || !all(nzchar(popup))) stop('popup must be of same length as coords') # lazy evaluation!
   lng <- coords[,1L]
   lat <- coords[,2L]
   
   map_empty <- fitBounds(map = addTiles(leaflet()), lat1 = min(lat), lat2 = max(lat), lng1 = min(lng), lng2 = max(lng))
   map_popup <- addPopups(
-    map = map_empty, lng = lng, lat = lat, popup = popup,
-    options = popupOptions(closeButton = FALSE, closeOnClick = FALSE)
+    map = map_empty, lng = lng, lat = lat, popup = popup#,
+    #options = popupOptions(closeButton = FALSE, closeOnClick = FALSE)
   )
   return(map_popup)
+  
 }
 
