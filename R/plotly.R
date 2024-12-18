1a1640d8cb979f69a2f79cc100ff44aa8c8bdd0e
Tingting Zhan <tingtingzhan@gmail.com>
2024-12-18 11:23:16 -0500

    test

--- R/plotly.R
+++ R/plotly.R
@@ -41,34 +41,31 @@ turn_coords <- function(coords, ...) {
     n <- dim(coords[[i]])[1L]
     if (n <= 1L) stop('wont allow')
 
-    p1 <- add_segments(
-      p = p1,
-      x = lon[seq_len(n-1L)], xend = lon[2:n],
-      y = lat[seq_len(n-1L)], yend = lat[2:n],
-      line = list(color = col[i], width = 2),
-      #hoverinfo = 'none'
-      text = rep('abc', n-1L), hoverinfo = 'text'
-      # https://stackoverflow.com/questions/63458372/plotly-add-segment-with-tooltip-along-entire-segment
-      # https://github.com/plotly/plotly.R/issues/1832#issuecomment-675721763
-      # TL;DR: plotly cannot do this, as of Sep 2024
-    )
-    
-    p1 <- add_markers(
-      p = p1,
-      x = lon, y = lat, text = nm,
-      marker = list(color = col[i]),
-      hoverinfo = 'text', 
-      hoverlabel = list(
-        font = list(
-          color = 'white'
-        ), 
-        #bordercolor = 'white' # default 'black' 
-        bordercolor = col[i]
+    p1 <- p1 |>
+      add_segments(
+        x = lon[seq_len(n-1L)], xend = lon[2:n],
+        y = lat[seq_len(n-1L)], yend = lat[2:n],
+        line = list(color = col[i], width = 2),
+        #hoverinfo = 'none'
+        text = rep('abc', n-1L), hoverinfo = 'text'
+        # https://stackoverflow.com/questions/63458372/plotly-add-segment-with-tooltip-along-entire-segment
+        # https://github.com/plotly/plotly.R/issues/1832#issuecomment-675721763
+        # TL;DR: plotly cannot do this, as of Sep 2024
+      ) |> 
+      add_markers(
+        # `add_markers` after `add_segments` !!
+        # it seems `hoverinfo` overwrites!!
+        x = lon, y = lat, text = nm,
+        marker = list(color = col[i]),
+        hoverinfo = 'text', 
+        hoverlabel = list(
+          font = list(
+            color = 'white'
+          ), 
+          #bordercolor = 'white' # default 'black' 
+          bordercolor = col[i]
+        )
       )
-    )
-    # `add_markers` after `add_segments` !!
-    # it seems `hoverinfo` overwrites!!
-    
   }
   
   # https://plotly.com/r/reference/layout/geo/
