7ba4e747c12c8b2ac14ecde382b6d333132a8651
Tingting Zhan <tingtingzhan@gmail.com>
2026-08-14 15:38:22 -0400

    tzh

--- R/0PACKAGE.R
+++ R/0PACKAGE.R
@@ -7,7 +7,6 @@
 #' \item{`airports_datahub`}{\url{https://github.com/datasets/airport-codes}}
 #' }
 #' 
-#' @importFrom methods new setClass setMethod callNextMethod
 #' @importClassesFrom sp SpatialPointsDataFrame
 '_PACKAGE'
 
